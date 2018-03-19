71e321d70153d3b746141250598dc10aab73c6db
Luis Herranz <luisherranz@gmail.com>
2018-03-19 17:33:25 +0100

    Working with latest

--- plugin.php
+++ plugin.php
@@ -16,7 +16,7 @@ class Yoast_To_REST_API {
 	function add_yoast_data() {
 		// Custom post types (including posts and pages):
 		$types = get_post_types(array('public' => true ));
-		foreach ( $types as $key => $type ) {
+		foreach ($types as $key => $type) {
 			register_rest_field($type, 'yoast_meta',
 				array(
 					'get_callback'    => array($this, 'wp_api_encode_yoast'),
@@ -28,7 +28,7 @@ class Yoast_To_REST_API {
 
 		// Add custom taxonomies (including category and tag).
 		$taxonomies = get_taxonomies(array('public' => true));
-		foreach ( $taxonomies as $key => $taxonomy ) {
+		foreach ($taxonomies as $key => $taxonomy) {
 			if ($taxonomy === 'post_tag') $taxonomy = 'tag';
 			register_rest_field($taxonomy, 'yoast_meta',
 				array(
@@ -40,37 +40,44 @@ class Yoast_To_REST_API {
 		}
 	}
 
-	function get_title_home_wpseo() {
+	function get_home_title() {
+    $wpseo_frontend = WPSEO_Frontend_To_REST_API::get_instance();
+    return $wpseo_frontend->get_title_from_options('title-home-wpseo');
+	}
+
+	function get_post_type_title($post_type) {
 		$wpseo_frontend = WPSEO_Frontend_To_REST_API::get_instance();
-		return $wpseo_frontend->get_title_from_options('title-home-wpseo');
+		$args = array('post_type' => $post_type);
+		$GLOBALS['wp_query'] = new WP_Query($args);
+		return $wpseo_frontend->public_get_post_type_archive_title();
 	}
 
 	function wp_api_encode_yoast( $p, $field_name, $request ) {
 		$wpseo_frontend = WPSEO_Frontend_To_REST_API::get_instance();
-		$args = array(
-  		'p' => $p['id'],
-  		'post_type' => 'any'
-		);
-		$GLOBALS['wp_query'] = new WP_Query( $args );
+		$args = array('p' => $p['id'], 'post_type' => 'any');
+		$GLOBALS['wp_query'] = new WP_Query($args);
 		return array(
 			'title' => $wpseo_frontend->get_content_title(),
 		);
 	}
 
 	function wp_api_encode_yoast_taxonomy($tag){
-	  $args = array(
-	   'tag_id' => $tag['id'],
-	  );
-	  $GLOBALS['wp_query'] = new WP_Query( $args );
 		$wpseo_frontend = WPSEO_Frontend_To_REST_API::get_instance();
+	  $args = array('tag_id' => $tag['id']);
+	  $GLOBALS['wp_query'] = new WP_Query($args);
 		return array(
 			'title' => $wpseo_frontend->get_taxonomy_title(),
 		);
 	 }
 
 	function add_yoast_to_latest($data) {
+		if ($data['id'] === 'post') {
+			$title = $this->get_home_title();
+		} else {
+			$title = $this->get_post_type_title($data['id']);
+		}
 		$data['yoast_meta'] = array(
-			'title' => $this->get_title_home_wpseo(),
+			'title' => $title
 		);
 		return $data;
 	}
